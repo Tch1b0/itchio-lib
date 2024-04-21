e03a35ec8fb629de41f1fb093be9d5cd6e2c23e2
Tch1b0 <Jpour3006@gmail.com>
2024-04-21 14:43:05 +0200

    fix dict iteration

--- itchio/utility.py
+++ itchio/utility.py
@@ -5,7 +5,7 @@ def satisfy_dict(have_dict: dict, want_dict: dict) -> dict:
     If a key is missing, the want_dict value is assigned to the key.
     """
     new_dict = have_dict.copy()
-    for key, value in want_dict:
+    for key, value in want_dict.items():
         if key not in have_dict:
             new_dict[key] = value
     return new_dict
